dfd5b94780bafce623589ac4426137419bac62d7
Neringa Siozinyte <neringa8809@gmail.com>
2025-03-13 12:16:41 +0100

    l

--- models/staging/staging_flights_two_month.sql
+++ models/staging/staging_flights_two_month.sql
@@ -2,7 +2,7 @@
 
 WITH flights_two_month AS (
     SELECT * 
-    FROM {{source('flights_data', 'flights_selected')}}
+    FROM {{source('flights_data', 'flights_all_jan_feb_2024')}}
     WHERE DATE_PART('month', flight_date) = 2 
 )
 SELECT * FROM flights_two_month
